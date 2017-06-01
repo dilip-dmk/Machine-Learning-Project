201277c6251160e26f05c623c16a34982e575582
wdxtub <dacrocodilee@gmail.com>
2017-06-01 08:32:28 +0800

    comment update

--- tensorflow_guide/01_mnist.py
+++ tensorflow_guide/01_mnist.py
@@ -6,6 +6,7 @@ import os
 from six.moves.urllib.request import urlretrieve
 import tensorflow as tf
 
+# 这里需要翻墙，不然下载巨慢
 SOURCE_URL = 'http://yann.lecun.com/exdb/mnist/'
 WORK_DIRECTORY = "./mnist-data"
 
@@ -23,6 +24,7 @@ def maybe_download(filename):
         print('Already downloaded', filename)
     return filepath
 
+# 这里把所有的训练数据都搞下来
 train_data_filename = maybe_download('train-images-idx3-ubyte.gz')
 train_labels_filename = maybe_download('train-labels-idx1-ubyte.gz')
 test_data_filename = maybe_download('t10k-images-idx3-ubyte.gz')
@@ -30,6 +32,7 @@ test_labels_filename = maybe_download('t10k-labels-idx1-ubyte.gz')
 
 # 如果遇到 MacOS 的 Python as a framework 的问题，参考
 # https://stackoverflow.com/questions/29433824/unable-to-import-matplotlib-pyplot-as-plt-in-virtualenv
+# 这里我们先看看数据集里有什么，只是一个展示，并不会对图片进行预处理
 def sanity_check():
     with gzip.open(test_data_filename) as f:
         # Print the header fields.
@@ -48,6 +51,7 @@ def sanity_check():
         print('First 10 pixels:', image[:10])
 
         # We'll show the image and its pixel value histogram side-by-side.
+        # 输出原始图片和直方图，来看看具体的样子
         _, (ax1, ax2) = plt.subplots(1, 2)
 
         # To interpret the values as a 28x28 image, we need to reshape
@@ -56,7 +60,20 @@ def sanity_check():
         ax2.hist(image, bins=20, range=[0,255])
         plt.show()
 
+        # 这里是把 [0, 255] 映射到 [-0.5, 0.5] 之后的展示
+        # Let's convert the uint8 image to 32 bit floats and rescale 
+        # the values to be centered around 0, between [-0.5, 0.5]. 
+        # 
+        # We again plot the image and histogram to check that we 
+        # haven't mangled the data.
+        scaled = image.astype(numpy.float32)
+        scaled = (scaled - (255 / 2.0)) / 255
+        _, (ax1, ax2) = plt.subplots(1, 2)
+        ax1.imshow(scaled.reshape(28, 28), cmap=plt.cm.Greys)
+        ax2.hist(scaled, bins=20, range=[-0.5, 0.5])
+        plt.show()
 
+        # 这里读取 Label，也是一个测试而已
     with gzip.open(test_labels_filename) as f:
         # Print the header fields.
         for field in ['magic number', 'label count']:
@@ -64,11 +81,14 @@ def sanity_check():
 
         print('First label:', struct.unpack('B', f.read(1))[0]) 
 
+# 简单显示一下，然后进行之后的步骤
 sanity_check()
 
+# 处理图片数据
 IMAGE_SIZE = 28
 PIXEL_DEPTH = 255
 
+# 这个函数会提取并处理数据
 def extract_data(filename, num_images):
     """Extract the images into a 4D tensor [image index, y, x, channels].
   
@@ -90,13 +110,14 @@ def extract_data(filename, num_images):
 train_data = extract_data(train_data_filename, 60000)
 test_data = extract_data(test_data_filename, 10000)
 
-
+# 这里把处理后的输出展示下
 print('Training data shape', train_data.shape)
 _, (ax1, ax2) = plt.subplots(1, 2)
 ax1.imshow(train_data[0].reshape(28, 28), cmap=plt.cm.Greys)
 ax2.imshow(train_data[1].reshape(28, 28), cmap=plt.cm.Greys)
 plt.show()
 
+# 接下来处理标签，我们需要把类别处理成向量，如果是第二类，那么对应 [0,1,0,...,0]，即第二个位置为 1
 NUM_LABELS = 10
 
 def extract_labels(filename, num_images):
@@ -113,11 +134,12 @@ def extract_labels(filename, num_images):
 train_labels = extract_labels(train_labels_filename, 60000)
 test_labels = extract_labels(test_labels_filename, 10000)
 
+# 同样测试一下数据
 print('Training labels shape', train_labels.shape)
 print('First label vector', train_labels[0])
 print('Second label vector', train_labels[1])
 
-
+# 这里我们把数据分成训练、测试和验证集
 VALIDATION_SIZE = 5000
 
 validation_data = train_data[:VALIDATION_SIZE, :, :, :]
@@ -130,58 +152,53 @@ train_size = train_labels.shape[0]
 print('Validation shape', validation_data.shape)
 print('Train size', train_size)
 
-'''
-Now that we've prepared our data, we're ready to define our model.
 
-The comments describe the architecture, which fairly typical of models 
-that process image data. The raw input passes through several convolution 
-and max pooling layers with rectified linear activations before several fully 
-connected layers and a softmax loss for predicting the output class. 
-During training, we use dropout.
+# 这里开始定义模型
+# 从原始输入开始，进行卷积(convolution)和池化(max pooling)处理，在全连接层之前会用 ReLU 
+# 作为激活函数，最后用 softmax 来处理输出，把类别信息转化成概率，训练的时候使用 Dropout
+#
+# 准备模型可以分三步
+# 1. 定义变量，来保存我们要训练的权重 weights
+# 2. 定义模型的图结构
+# 3. 把模型的图分别用于训练、测试和验证（复制几份）
 
-We'll separate our model definition into three steps:
-
-1. Defining the variables that will hold the trainable weights.
-2. Defining the basic model graph structure described above. And,
-3. Stamping out several copies of the model graph for training, testing, and validation.
-
-We'll start with the variables.
-'''
-
-# We'll bundle groups of examples during training for efficiency.
-# This defines the size of the batch.
+# 先处理好变量
+# 从效率的角度考虑，我们会把样本分组，这里是一个组的样本数量
 BATCH_SIZE = 60
-# We have only one channel in our grayscale images.
+# 因为是灰度图，所以只有一个通道 channel
 NUM_CHANNELS = 1
-# The random seed that defines initialization.
+# 固定随机种子，保证每次的结果一致（不然没办法验证数据和模型）
 SEED = 42
 
-# This is where training samples and labels are fed to the graph.
-# These placeholder nodes will be fed a batch of training data at each
-# training step, which we'll write once we define the graph structure.
+# 我们在这里把训练数据和类别标签『喂』给模型，不过这里只是一个占位符(placeholder)
+# 真正训练的时候，这些节点在每一步会获取批量数据
 train_data_node = tf.placeholder(
   tf.float32,
   shape=(BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, NUM_CHANNELS))
 train_labels_node = tf.placeholder(tf.float32,
                                    shape=(BATCH_SIZE, NUM_LABELS))
 
-# For the validation and test data, we'll just hold the entire dataset in
-# one constant node.
+# 对于验证和测试数据，直接保存到一个常量节点里即可（不存在训练的过程，不需要是变量）
 validation_data_node = tf.constant(validation_data)
 test_data_node = tf.constant(test_data)
 
-# The variables below hold all the trainable weights. For each, the
-# parameter defines how the variables will be initialized.
+# 下面的变量保存着所有的需要训练的权重。后面的参数定义了这些变量的初始化条件
+# 用高斯分布初始化卷积的 weights
 conv1_weights = tf.Variable(
   tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
                       stddev=0.1,
                       seed=SEED))
+# 初始的 bias 为 0
 conv1_biases = tf.Variable(tf.zeros([32]))
+# 第二层的卷积权重，32 个输入（对应上面的 32），然后下面是 64 维
 conv2_weights = tf.Variable(
   tf.truncated_normal([5, 5, 32, 64],
                       stddev=0.1,
                       seed=SEED))
+# 同理，bias 也是 64 维，但是这里用 0.1
 conv2_biases = tf.Variable(tf.constant(0.1, shape=[64]))
+# 然后是一个全连接的网络，共 512 维，为什么呢，因为我们有卷积和池化的存在，所以是 32*64/4
+# (?这里我也不是很确定)
 fc1_weights = tf.Variable(  # fully connected, depth 512.
   tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512],
                       stddev=0.1,
@@ -193,7 +210,7 @@ fc2_weights = tf.Variable(
                       seed=SEED))
 fc2_biases = tf.Variable(tf.constant(0.1, shape=[NUM_LABELS]))
 
-print('Set Variable Done')
+print('变量设置完毕')
 
 '''
 Now that we've defined the variables to be trained, we're ready to wire them 
